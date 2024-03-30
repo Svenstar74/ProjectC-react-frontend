a2fec46737f191ac5dad9c2f2d2b900aa66299a4
Svenstar74 <sven.firmbach@gmx.de>
2024-03-30 15:26:37 +0100

    remove highlights when search is emptied

--- src/features/search/components/AutocompleteClimateConceptSearch.tsx
+++ src/features/search/components/AutocompleteClimateConceptSearch.tsx
@@ -81,6 +81,7 @@ function AutocompleteClimateConceptSearch(
         onChange(newFilteredOptions);
       }
     } else {
+      highlightNodes([]);
       setShowOptions(false);
     }
   }, [inputValue]);
